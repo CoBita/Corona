841647e48439a1913b51d319800505cd1deca344
박상혁 <psh161@naver.com>
2020-05-15 10:58:03 +0900

    -git Commit

---
+++ app/src/main/java/com/bita/corona/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bita.corona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.and.base.log.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(Const.SERVICE_KEY == Const.SERVICE_KEY2)
+    }
+}
---
+++ app/src/main/java/com/bita/corona/base/CApplication.kt
@@ -0,0 +1,5 @@
+package com.bita.corona.base
+
+import com.and.base.ui.BaseApplication
+
+class CApplication : BaseApplication()
